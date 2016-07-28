0418f109df4d08895a06612d3969c10a3e7207f0
crazybits <273593465@qq.com>
2016-07-28 18:31:54 +0800

    add test cases

---
+++ src/test/java/p2p/ClientTest.java
@@ -0,0 +1,36 @@
+/*
+ * COPYRIGHT. HSBC HOLDINGS PLC 2016. ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of HSBC Holdings plc.
+ */
+package p2p;
+
+import net.p2p.client.P2PClient;
+import net.p2p.protocol.Peer;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import config.SpringConfig;
+
+/**
+ * <p>
+ * <b> TODO : Insert description of the class's responsibility/role. </b>
+ * </p>
+ */
+public class ClientTest extends P2PClient {
+
+    public static void main(final String[] args) {
+
+        AnnotationConfigApplicationContext springContext = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        ClientTest clientTest = new ClientTest();
+
+        clientTest.connect(new Peer("127.0.0.1", 8888));
+
+
+    }
+}
---
+++ src/test/java/p2p/ServerTest.java
@@ -0,0 +1,43 @@
+/*
+ * COPYRIGHT. HSBC HOLDINGS PLC 2016. ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of HSBC Holdings plc.
+ */
+package p2p;
+
+import net.p2p.server.P2PServer;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import config.SpringConfig;
+
+/**
+ * <p>
+ * <b> TODO : Insert description of the class's responsibility/role. </b>
+ * </p>
+ */
+public class ServerTest extends P2PServer {
+
+    /**
+     * <p>
+     * <b> TODO : Insert description of the method's responsibility/role. </b>
+     * </p>
+     * 
+     * @param args
+     */
+    public static void main(final String[] args) {
+
+        AnnotationConfigApplicationContext springContext = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        ServerTest serverTest = new ServerTest();
+
+        Thread serverThread = new Thread(serverTest);
+        serverThread.start();
+
+    }
+
+}
