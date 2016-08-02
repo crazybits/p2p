bf09875d9de2e2ecaacfcf5b8622d77dbc4b1a6c
crazybits <273593465@qq.com>
2016-08-02 14:41:08 +0800

    remove timeout handler

--- src/main/java/net/p2p/server/P2PServerChannelInitializer.java
+++ src/main/java/net/p2p/server/P2PServerChannelInitializer.java
@@ -4,10 +4,6 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.timeout.IdleStateHandler;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-
-import java.util.concurrent.TimeUnit;
-
 import net.p2p.peerdiscovery.PeerDiscoveryManager;
 
 import org.slf4j.Logger;
@@ -30,7 +26,8 @@ public class P2PServerChannelInitializer extends ChannelInitializer<NioSocketCha
         P2PServerChannelInitializer.logger.debug("init handler");
 
         ch.pipeline().addLast("netty logging", new LoggingHandler());
-        ch.pipeline().addLast("time out handler", new ReadTimeoutHandler(61, TimeUnit.SECONDS));
+        // ch.pipeline().addLast("time out handler", new
+        // ReadTimeoutHandler(100, TimeUnit.SECONDS));
         ch.pipeline().addLast("ping pong", new IdleStateHandler(60, 60, 60));
         ch.pipeline().addLast("marshalling Decoder", MarshallingFactory.buildMarshallingDecoder());
         ch.pipeline().addLast("marshalling Encoder", MarshallingFactory.buildMarshallingEncoder());
